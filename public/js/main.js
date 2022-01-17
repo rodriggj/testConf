ccb859f05c7088c692db4164de13a6fcdf50375e
willhpro <94105960+willhpro@users.noreply.github.com>
2022-01-17 16:55:38 +0000

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 const SF_BASE_URI = `https://e2edemo.my.salesforce.com`
-const SF_BEARER_TOKEN = `00D5f000003vd1P!ARUAQEyYTsN.VBAMjztVlGICcpxRun7r7jKpyDkMgwtWAvzLR9v4tzTDBQWsmLDdEMbKwLenUhW8y4HkeamI.kVBqdjmlP8H`
+const SF_BEARER_TOKEN = `00D5f000003vd1P!ARUAQFGHiDpmzHoA0iwr4pV0RiD.UthM7q7dfhj3vjukx75GcH6Szc4B1NHmSksd.uduIN7bOHgYgRskfcR23yFhAylT4g0j`
 
 window.addEventListener('load', (event) => {
   var speaker_data = null;
